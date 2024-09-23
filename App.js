25c7275d5210fe212a1030a352ca1ddfbd1b9f69
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2024-09-24 04:27:00 +0530

    created functional components

--- App.js
+++ App.js
@@ -2,11 +2,27 @@ import React from "react";
 import  ReactDOM  from "react-dom/client";
 
 
-const jsxHeading = (
-    <h1 id="header"   className="header">
-        Hello, World! from JSX 🚀
-    </h1>
+
+// functional component
+const ParagraphComponent = ()=> (
+    <p id="paragraph">
+        This is a paragraph component!!
+    </p>
+);
+
+const elem = <p>this is paragraph element</p>
+
+const HeadingComponent= () => (
+    <div>
+        <h1 id="header"className="heading">
+            This is a heading component!!
+        </h1>
+        <ParagraphComponent /> // we can write like this.
+        <ParagraphComponent></ParagraphComponent> // we can write like this as well.
+        {elem} // we can put any JS code inside the JSX.
+    </div>
+
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
