1c5a5095818e9d2e26dc526d183f2d1ca358d4a6
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2024-09-23 12:57:52 +0530

    tinkering with createElement

--- App.js
+++ App.js
@@ -1,8 +1,40 @@
-        const heading = React.createElement("h1", {}, "Hello, world! from react.");
-        const root = ReactDOM.createRoot(document.getElementById("root")); //  ye bolega ki kiske andar render hona hai, uska id dedo
-        root.render(heading);
+        /**
+         * <div id="parent">
+         *     <div id="child">
+         *          <h1> </h1>
+         *     </div>
+         *     <div id="child2">
+         *          <h1> </h1>
+         *     </div>
+         * </div>
+         * 
+         * 
+         * 
+         * React.createElement gives ReactElement(object) => HTML (browser understands)
+         */
+        
+        const parent = React.createElement("div", {id:"parent"},[
+
+        React.createElement("div", {key:2,id:"child"}, 
+        [React.createElement("h1",{key:234},"dum dabak dum dum"),
+        React.createElement("h2",{key:345},"i am sibling of h1 tag")
+    ]),
+     React.createElement("div", {key:3,id:"child2"}, 
+        [React.createElement("h1",{key:236},"dum dabak dum dum"),
+        React.createElement("h2",{key:347},"i am sibling of h1 tag")
+    ])
+
+    ]);
+
+
+        
+        
+        // const heading = React.createElement("h1", {}, "Hello, world! from react.");
+        const root = ReactDOM.createRoot(document.getElementById("root")); //  ye bolega ki kiske andar render hona hai(kisko root node banaye?), uska id dedo
+        // root.render(heading);
+        root.render(parent);
 
 
         const paragraph = React.createElement("p", {}, "this is a paragraph element created using react");
-        const para = ReactDOM.createRoot(document.getElementById("para")); // ye bolega ki kiske andar render hona hai, uska id dedo
+        const para = ReactDOM.createRoot(document.getElementById("para")); // ye bolega ki kiske andar render hona hai(kisko root node banaye?), uska id dedo
         para.render(paragraph);
